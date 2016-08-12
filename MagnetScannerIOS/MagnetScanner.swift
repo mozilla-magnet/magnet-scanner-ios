0358d25f5f3b6978ec24632910f2c38faa63669e
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2016-08-12 12:11:42 +0200

    Initial commit

---
+++ MagnetScannerIOS/MagnetScanner.swift
@@ -0,0 +1,19 @@
+//
+//  MagnetScanner.swift
+//  MagnetScannerIOS
+//
+//  Created by Fernando Jiménez on 8/12/16.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+import Foundation
+
+public class MagnetScanner {
+    public func start() {
+        print("MAGNET START");
+    }
+    
+    public func stop() {
+        print("MAGNET STOP");
+    }
+}
---
+++ MagnetScannerIOS/MagnetScannerIOS.h
@@ -0,0 +1,19 @@
+//
+//  MagnetScannerIOS.h
+//  MagnetScannerIOS
+//
+//  Created by Fernando Jiménez on 8/12/16.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagnetScannerIOS.
+FOUNDATION_EXPORT double MagnetScannerIOSVersionNumber;
+
+//! Project version string for MagnetScannerIOS.
+FOUNDATION_EXPORT const unsigned char MagnetScannerIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagnetScannerIOS/PublicHeader.h>
+
+
